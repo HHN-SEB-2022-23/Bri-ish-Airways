1170e1c16a2b10b522065ea01d44b5a3a20e2e14
BLVD3 <00nvogel@gmail.com>
2023-03-17 20:33:00 +0100

    Implemented Pilot.java

--- src/de/hhn/britishAirways/Pilot.java
+++ src/de/hhn/britishAirways/Pilot.java
@@ -1,4 +1,41 @@
 package de.hhn.britishAirways;
 
+/**
+ * @author Nico Vogel
+ * @version 0.1
+ */
+
 public class Pilot {
+    private final String name;
+    private Airline employer;
+
+    public Pilot(String name) {
+        this.name = name;
+        System.out.println(this + " created");
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Airline getEmployer() {
+        return this.employer;
+    }
+
+    public void setEmployer(Airline employer) {
+        this.employer = employer;
+    }
+
+    public void hire() {
+        System.out.println(this + " is getting hired.");
+    }
+
+    public void fire() {
+        System.out.println(this + " is getting fired.");
+    }
+
+    @Override
+    public String toString() {
+        return "Pilot " + name;
+    }
 }
