03ef395c6bd3d95b6c060538a8f9890b45be8f33
fritz-pal <henri@staudenrausch.de>
2023-03-17 17:43:27 +0100

    Airport

--- src/de/hhn/britishAirways/Airport.java
+++ src/de/hhn/britishAirways/Airport.java
@@ -1,4 +1,38 @@
 package de.hhn.britishAirways;
 
+
 public class Airport {
+    private String IATACode;
+    private String name;
+
+    public Airport(String IATACode, String name) {
+        this.IATACode = IATACode;
+        this.name = name;
+        System.out.println("Airport created: " + this);
+    }
+
+    public String getIATACode() {
+        return IATACode;
+    }
+
+    public void setIATACode(String IATACode) {
+        this.IATACode = IATACode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Airport " + name + "(" + IATACode + ")";
+    }
+
+    public void deice(){
+        System.out.println(this + " is deicing");
+    }
 }
---
+++ src/de/hhn/britishAirways/Main.java
@@ -0,0 +1,8 @@
+package de.hhn.britishAirways;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Airport("STR", "Stuttgart");
+    }
+}
