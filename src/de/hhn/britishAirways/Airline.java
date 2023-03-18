aef0853eb2bcf5795bbfdbf798270725de5b39f6
René Ott <rene.ott1997@yahoo.de>
2023-03-18 23:17:11 +0100

    Airline implemented

--- src/de/hhn/britishAirways/Airline.java
+++ src/de/hhn/britishAirways/Airline.java
@@ -1,4 +1,103 @@
 package de.hhn.britishAirways;
 
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author René Ott
+ */
 public class Airline {
+
+    private String IATACode;
+
+    private String name;
+
+    private Set<Pilot> pilots = new HashSet(50);    //Approx. no. of pilots employed by BA is 3200. No. is too high for initial capacity of the HashSet though
+
+    private Set<Flight> flights= new HashSet(50);
+
+    private Set<Plane> planes= new HashSet(20);
+
+    public Airline(String IATACode, String name){
+        this.IATACode = IATACode;
+        this.name = name;
+        System.out.println(this + " created");
+    }
+
+    public String getIATACode(){
+        return IATACode;
+    }
+
+    public void setIATACode(String IATACode){
+        this.IATACode = IATACode;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String toString(){
+        return "Airline " + name + " (" + IATACode + ")";
+    }
+
+    public void hire(Pilot pilot){
+        System.out.println(pilot + " is hired");
+    }
+
+    public void fire(Pilot pilot){
+        System.out.println(pilot + " is fired");
+    }
+
+    public boolean addPilot(Pilot pilot){
+        return this.pilots.add(pilot);
+    }
+
+    public boolean removePilot(Pilot pilot){
+        return this.pilots.remove(pilot);
+    }
+
+    public boolean hasPilot(Pilot pilot){
+        return this.pilots.contains(pilot);
+    }
+
+    public void clearPilots(){
+        this.pilots.clear();
+    }
+
+    public boolean addFlight(Flight flight){
+        return this.flights.add(flight);
+    }
+
+    public boolean removeFlight(Flight flight){
+        return this.flights.remove(flight);
+    }
+
+    public boolean hasFlight(Flight flight){
+        return this.flights.contains(flight);
+    }
+
+    public void clearFlights(){
+        this.flights.clear();
+    }
+
+    public boolean addPlane(Plane plane){
+        return this.planes.add(plane);
+    }
+
+    public boolean removePlane(Plane plane){
+        return this.planes.remove(plane);
+    }
+
+    public boolean hasPlane(Plane plane){
+        return this.planes.contains(plane);
+    }
+
+    public void clearPlanes(){
+        this.planes.clear();
+    }
 }
--- src/de/hhn/britishAirways/Main.java
+++ src/de/hhn/britishAirways/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         var str = new Airport("STR", "Stuttgart");
         var lhr = new Airport("LHR", "London Heathrow");
-        var ba = new Airline("BA", "British Airways"); // <- bitte konstruktor korrigieren (falls nötig), ist nur geraten
+        var ba = new Airline("BA", "British Airways");
         var strToLhr = new Flight(ba, "BA123");
         strToLhr.setDeparture(LocalDateTime.of(2019, 1, 1, 10, 0));
     }
