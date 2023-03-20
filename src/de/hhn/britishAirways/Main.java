f3c8a442fbc15359830d9ed97823581023a9c471
fritz-pal <henri@staudenrausch.de>
2023-03-20 12:19:39 +0100

    Airport.java Kommentare

--- src/de/hhn/britishAirways/Main.java
+++ src/de/hhn/britishAirways/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Airport str = new Airport("STR", "Stuttgart", null);
         Airport lhr = new Airport("LHR", "London Heathrow", null);
+        str.deice();
         Airline ba = new Airline("BA", "British Airways");
         Flight strToLhr = new Flight(ba, "BA123");
         strToLhr.setDeparture(LocalDateTime.of(2019, 1, 1, 10, 0));
