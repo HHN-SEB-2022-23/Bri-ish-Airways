5ba0cd2e1296985782a45d265386f2212d6f7cbd
Jannik Danecker <danecker.jannik@gmail.com>
2023-03-20 11:59:14 +0100

    Implemented Passenger

--- src/de/hhn/britishAirways/Passenger.java
+++ src/de/hhn/britishAirways/Passenger.java
@@ -1,4 +1,47 @@
 package de.hhn.britishAirways;
 
+/**
+ * @author jannikdanecker
+ */
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Passenger {
+    private final String name;
+
+
+    private final Map<Flight, Seat> bookings = new HashMap<>();
+
+
+    public Passenger(String name) {
+        this.name = name;
+    }
+
+    public Flight[] getflights() {
+        return bookings.keySet().toArray(new Flight[0]);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Seat getSeat(Flight flight) {
+        return bookings.get(flight);
+    }
+
+    public void addBooking(Flight flight, Seat seat){
+        bookings.put(flight, seat);
+    }
+
+    public void removeBooking(Flight flight){
+        bookings.remove(flight);
+    }
+
+    @Override
+    public String toString() {
+        return "Passenger " + name;
+    }
 }
