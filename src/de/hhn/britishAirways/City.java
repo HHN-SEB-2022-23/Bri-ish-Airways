9a50632506f40fcc6d13e61d61cdc0b43cffd39c
KirbyHasAGun <felixmarzioch@gmail.com>
2023-03-20 12:14:54 +0100

    City geschrieben

--- src/de/hhn/britishAirways/City.java
+++ src/de/hhn/britishAirways/City.java
@@ -1,4 +1,35 @@
 package de.hhn.britishAirways;
 
+/**
+ * @author Felix Marzioch
+ */
+
+import java.util.HashSet;
+
 public class City {
+
+    private final String name;
+    private final HashSet<Airport> airports = new HashSet<>();
+
+    public City(String name) {
+        this.name = name;
+        System.out.println(this + " created");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addAirport (Airport airport){
+        airports.add(airport);
+
+    }
+    public Airport[] getAirports() {
+        return airports.toArray(Airport[]::new);
+    }
+
+    @Override
+    public String toString() {
+        return "City " + name;
+    }
 }
