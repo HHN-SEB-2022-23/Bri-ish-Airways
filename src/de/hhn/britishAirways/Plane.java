f9824a3b75e94705de6fd79f37cc06f0bf0124bb
yagmursimsek <ysimsek@hs-heilbronn.de>
2023-03-20 11:27:31 +0100

    - initialisieren der Klasse Plane - erstellen von getter/setter

--- src/de/hhn/britishAirways/Plane.java
+++ src/de/hhn/britishAirways/Plane.java
@@ -1,4 +1,60 @@
 package de.hhn.britishAirways;
 
+/**
+ * @author Yagmur Simsek
+ * @version 0.1
+ */
+
 public class Plane {
+
+    public String model;
+    public int serialNum;
+    public int hoursFlown;
+    public int tailNr;
+
+    public Plane (String model, int serialNum, int hoursFlown, int tailNr) {
+
+        this.model = model;
+        this.serialNum = serialNum;
+        this.hoursFlown = hoursFlown;
+        this.tailNr = tailNr;
+
+    }
+
+    public String getModel() {
+        return model;
+
+    }
+
+    public void setModel (String model) {
+        this.model = model;
+
+    }
+
+    public int getSerialNum() {
+        return serialNum;
+
+    }
+
+    public void setSerialNum (int serialNum) {
+        this.serialNum = serialNum;
+    }
+
+    public int getHoursFlown(int hoursFlown) {
+        return hoursFlown;
+    }
+
+    public void setHoursFlown(int hoursFlown) {
+        this.hoursFlown = hoursFlown;
+    }
+
+    public int getTailNr() {
+        return tailNr;
+    }
+
+    public void setTailNr(int tailNr) {
+        this.tailNr = tailNr;
+    }
 }
+
+
