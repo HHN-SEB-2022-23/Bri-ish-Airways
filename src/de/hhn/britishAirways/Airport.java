ed54a7792a496f390475e5db1ef76fff9f44de2c
fritz-pal <henri@staudenrausch.de>
2023-03-20 11:50:31 +0100

    Airport.java Variablen für City und Flights hinzugefügt

--- src/de/hhn/britishAirways/Airport.java
+++ src/de/hhn/britishAirways/Airport.java
@@ -1,13 +1,27 @@
 package de.hhn.britishAirways;
 
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Airport {
+    private final Set<City> cities = new HashSet<>(3);
+    private final Set<Flight> arrivingFlights = new HashSet<>();
+    private final Set<Flight> departingFlights = new HashSet<>();
     private String IATACode;
     private String name;
 
-    public Airport(String IATACode, String name) {
+    public Airport(String IATACode, String name, City[] cities) {
         this.IATACode = IATACode;
         this.name = name;
+        if (cities != null) {
+            if(cities.length > 3){
+                throw new IllegalStateException("Airport can only have 3 cities or less");
+            }
+            for (City c : cities) {
+                addCity(c);
+            }
+        }
         System.out.println("Airport created: " + this);
     }
 
@@ -27,12 +41,51 @@ public class Airport {
         this.name = name;
     }
 
+    public void addCity(City city) {
+        if(cities.size() >= 3){
+            throw new IllegalStateException("Airport can only have 3 cities or less");
+        }
+        cities.add(city);
+    }
+
+    public void removeCity(City city) {
+        cities.remove(city);
+    }
+
+    public void addDepartingFlight(Flight flight) {
+        departingFlights.add(flight);
+    }
+
+    public void removeDepartingFlight(Flight flight) {
+        departingFlights.remove(flight);
+    }
+
+    public void addArrivingFlight(Flight flight) {
+        arrivingFlights.add(flight);
+    }
+
+    public void removeArrivingFlight(Flight flight) {
+         arrivingFlights.remove(flight);
+    }
+
+    public Set<City> getCities() {
+        return cities;
+    }
+
+    public Set<Flight> getDepartingFlights() {
+        return departingFlights;
+    }
+
+    public Set<Flight> getArrivingFlights() {
+        return arrivingFlights;
+    }
+
     @Override
     public String toString() {
         return "Airport " + name + "(" + IATACode + ")";
     }
 
-    public void deice(){
+    public void deice() {
         System.out.println(this + " is deicing");
     }
 }
--- src/de/hhn/britishAirways/Main.java
+++ src/de/hhn/britishAirways/Main.java
@@ -4,10 +4,10 @@ import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
-        var str = new Airport("STR", "Stuttgart");
-        var lhr = new Airport("LHR", "London Heathrow");
-        var ba = new Airline("BA", "British Airways");
-        var strToLhr = new Flight(ba, "BA123");
+        Airport str = new Airport("STR", "Stuttgart", null);
+        Airport lhr = new Airport("LHR", "London Heathrow", null);
+        Airline ba = new Airline("BA", "British Airways");
+        Flight strToLhr = new Flight(ba, "BA123");
         strToLhr.setDeparture(LocalDateTime.of(2019, 1, 1, 10, 0));
         strToLhr.setArrival(LocalDateTime.of(2019, 1, 1, 12, 0));
     }
